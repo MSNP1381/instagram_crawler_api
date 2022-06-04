0dd5da4f35641e03a658425525d7638fb5485208
MSNP1381 <mohamadnematpoor@gmail.com>
2022-06-04 11:03:24 +0430

    noyann to x1

--- x1/wsgi.py
+++ x1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'noyan_instagram_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'x1.settings')
 
 application = get_wsgi_application()
